a78c357ef97c77a1d1798faf775017200be7da6c
wrihat <479689451@qq.com>
2024-12-03 14:39:33 +0800

    TwoPageSwitcher implement

---
+++ TwoPageSwitcher/TwoPageSwitcher/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TwoPageSwitcher
+//
+//  Created by MeetYou on 2024/12/3.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ TwoPageSwitcher/TwoPageSwitcher/SecondViewController.h
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.h
+//  TwoPageSwitcher
+//
+//  Created by MeetYou on 2024/12/3.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SecondViewController : UIViewController
+-(IBAction)goToPage1:(id)sender;
+
+@end
+
+NS_ASSUME_NONNULL_END
